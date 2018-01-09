61fbec594b2997be6fe3293db469edd9345ca26e
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-09 11:21:45 +0000

    Create largest of three numbers.c

---
+++ largest of three numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf("%d is the largest number",a);
+}
+else(b>c)
+{
+printf("%d is largest number",b);
+}
+}
+else
+print("%d is largest number",c);
+}
