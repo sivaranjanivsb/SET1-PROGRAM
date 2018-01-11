c2f928c27b14929867d8bd7ca87f4eff9b1f38f3
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-11 10:32:15 +0000

    Create sum of natural no.c

---
+++ sum of natural no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+int sum=0;
+printf("enter the limit");
+scanf("%d",&n);
+for(i=o;i<n;i++)
+{
+sum=sum+i;
+}
+printf(" the sum of natural numbers is %d",sum);
+}
