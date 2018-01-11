c1af97b04a0f682de4e4427af1d396d9628012b8
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-11 10:53:18 +0000

    Create  sum of no in array.c

---
+++ sum of no in array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a[n],k,i,sum=0;
+printf(" enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i+=)
+{
+scanf("%d",&a[n]);
+}
+printf(" enter the limit");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+sum=sum+a[i];
+}
+printf(" the sum of number in an array for a given limit is %d",sum);
+}
